832b4377fdb4d28e5090b8388b854cbf3568a256
Илья <130059272+Licrua@users.noreply.github.com>
2024-11-19 22:51:07 +0300

    Update custom.js

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -6,11 +6,13 @@ function changeNotifications(el) {
     let img = el;
     console.log(el.src)
     let source = el.src.split('images/')[1];
+	console.log('source', source);
+	
     if (source === 'notice.svg') {
-        img.src = '../images/noticeOn.svg'
+        img.src = 'public/images/noticeOn.svg'
     } else if (source === 'noticeOn.svg') {
-        img.src = '../images/noticeOff.svg'
+        img.src = 'public/images/noticeOff.svg'
     } else {
-        img.src = '../images/notice.svg'
+        img.src = 'public/images/notice.svg'
     }
 }
